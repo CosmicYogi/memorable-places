441049a030b6892eeb20cd9541bea28dfa7313b6
Mitesh Soni <mitesh_soni@rocketmail.com>
2016-04-23 18:49:45 +0530

    Initial Commit

---
+++ Memorable Places/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Memorable Places
+//
+//  Created by mitesh soni on 23/04/16.
+//  Copyright © 2016 Mitesh Soni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
